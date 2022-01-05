cbe4179c60b484d1b095e51c0dcf90b9eb711c26
stephen-m-a <stephen_m_a@yahoo.com>
2022-01-05 11:21:12 +0000

    Moving scripts to changing order of styles

--- static/index.js
+++ static/index.js
@@ -1,7 +1,20 @@
-// Nav bar toggle
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+//Sign in a user
+function onSignIN(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token passed to the backend:
+    // var id_token = googleUser.getAuthResponse().id_token;
+    // console.log("ID Token: " + id_token);
+
+    window.location = './home.html'
 }
+
 
 // Sign out a user
 function signOut() {
@@ -10,3 +23,8 @@ function signOut() {
       console.log('User signed out.');
     });
   }
+
+  // Nav bar toggle
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
