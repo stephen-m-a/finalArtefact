77f1e841d5703c6d38b6429569d780e7e3cf54b2
stephen-m-a <stephen_m_a@yahoo.com>
2022-02-20 23:43:25 +0000

    Update Staff page and Discord embed

--- app.js
+++ app.js
@@ -11,6 +11,43 @@ app.use(express.static('static', ));
 //  })
  
 
+
+
+app.get('/editor', (req, res) => {
+  res.sendFile(path.join(initial_path, "editor.html"));
+})
+
+// upload link
+app.post('/upload', (req, res) => {
+  let file = req.files.image;
+  let date = new Date();
+  // image name
+  let imagename = date.getDate() + date.getTime() + file.name;
+  // image upload path
+  let path = 'public/uploads/' + imagename;
+
+  // create upload
+  file.mv(path, (err, result) => {
+      if(err){
+          throw err;
+      } else{
+          // our image upload path
+          res.json(`uploads/${imagename}`)
+      }
+  })
+})
+
+app.get("/:blog", (req, res) => {
+  res.sendFile(path.join(initial_path, "blog.html"));
+})
+
+app.use((req, res) => {
+  res.json("404");
+})
+
+
+
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, (err) => {
